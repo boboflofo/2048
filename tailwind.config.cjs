4ef9e949eb1b2c0fbd8262d020132c8438a0ecbd
boboflofo <113068890+boboflofo@users.noreply.github.com>
2023-03-15 23:20:20 -0700

    n

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx,ts,js,jsx}", "./2048/index.html"],
+  content: ["./src/**/*.{tsx,ts,js,jsx}", "./index.html"],
   theme: {
     extend: {},
   },
